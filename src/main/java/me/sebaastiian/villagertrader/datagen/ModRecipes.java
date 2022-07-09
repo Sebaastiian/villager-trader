951819cc798f439563fb14a7f47c3b33b61c1212
Sebaastiian <flockean11+github@gmail.com>
2022-07-09 14:18:02 +0200

    feat: Add recipes for Orb and Trading Station

--- src/main/java/me/sebaastiian/villagertrader/datagen/ModRecipes.java
+++ src/main/java/me/sebaastiian/villagertrader/datagen/ModRecipes.java
@@ -1,8 +1,14 @@
 package me.sebaastiian.villagertrader.datagen;
 
+import me.sebaastiian.villagertrader.common.VillagerTrader;
+import me.sebaastiian.villagertrader.setup.ModBlocks;
+import me.sebaastiian.villagertrader.setup.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraftforge.common.Tags;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
@@ -13,7 +19,26 @@ public class ModRecipes extends RecipeProvider {
     }
 
     @Override
-    protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
-        
+    protected void buildCraftingRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(ModItems.VILLAGER_ORB.get())
+                .pattern(" x ")
+                .pattern("xex")
+                .pattern(" x ")
+                .define('x', Tags.Items.LEATHER)
+                .define('e', Tags.Items.GEMS_EMERALD)
+                .group(VillagerTrader.MODID)
+                .unlockedBy("has_emerald", has(Tags.Items.GEMS_EMERALD))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped((ModBlocks.VILLAGER_TRADING_STATION.get()))
+                .pattern("ebe")
+                .pattern("ene")
+                .pattern("ebe")
+                .define('e', Tags.Items.GEMS_EMERALD)
+                .define('b', Tags.Items.STORAGE_BLOCKS_EMERALD)
+                .define('n', Tags.Items.NETHER_STARS)
+                .group(VillagerTrader.MODID)
+                .unlockedBy("has_villager_orb", has(ModItems.VILLAGER_ORB.get()))
+                .save(consumer);
     }
 }

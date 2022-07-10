339559f0811ba9ab12b73d5b9da2836c08755773
Sebaastiian <flockean11+github@gmail.com>
2022-07-10 09:22:40 +0200

    feat: Make villager show trade result in BER

--- src/main/java/me/sebaastiian/villagertrader/client/renderers/VillagerTradingStationRenderer.java
+++ src/main/java/me/sebaastiian/villagertrader/client/renderers/VillagerTradingStationRenderer.java
@@ -1,6 +1,7 @@
 package me.sebaastiian.villagertrader.client.renderers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.datafixers.util.Pair;
 import me.sebaastiian.villagertrader.common.blockentities.VillagerTradingStationBlockEntity;
 import me.sebaastiian.villagertrader.common.util.VillagerNbt;
 import net.minecraft.client.Minecraft;
@@ -9,11 +10,14 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class VillagerTradingStationRenderer implements BlockEntityRenderer<VillagerTradingStationBlockEntity> {
 
@@ -42,6 +46,10 @@ public class VillagerTradingStationRenderer implements BlockEntityRenderer<Villa
         Level level = blockEntity.getLevel();
         Villager villager = EntityType.VILLAGER.create(level);
         villager.load(villagerData);
+        List<Pair<Pair<ItemStack, ItemStack>, ItemStack>> offers = VillagerNbt.getOffers(villagerData);
+        Pair<Pair<ItemStack, ItemStack>, ItemStack> offer = offers.get(blockEntity.getSelectedTrade());
+        ItemStack result = offer.getSecond();
+        villager.setItemInHand(InteractionHand.MAIN_HAND, result);
 
         entityRenderDispatcher.render(villager, 0, 0, 0, 0F, partialTick,
                 poseStack, bufferSource, packedLight);
